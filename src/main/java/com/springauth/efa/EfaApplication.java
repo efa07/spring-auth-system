ece8c91e68ed355d185c9b01c01e4f0bf7d2cab7
efa <efatariku07@gmail.com>
2026-06-19 10:19:43 +0300

    first commit

---
+++ src/main/java/com/springauth/efa/EfaApplication.java
@@ -0,0 +1,13 @@
+package com.springauth.efa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springauth/efa/EfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springauth.efa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
